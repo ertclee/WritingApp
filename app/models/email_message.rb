55437efc9a68ba9b47c78c7a242ea2cf4aa2f4c9
kilim choi <stevn1202@berkeley.edu>
2014-09-23 10:59:45 -0700

    adding time

--- app/models/email_message.rb
+++ app/models/email_message.rb
@@ -8,6 +8,7 @@ class EmailMessage < ActiveRecord::Base
   
   def self.delay_email
     puts "does it enter the delay email?"
+    puts "time is ", Time.now.strftime("%H:%M")
     mailers = self.where("string_delivery_at = ?", Time.now.strftime("%H:%M"))
     puts "boolean is: ", mailers.present?
     if mailers.present?
