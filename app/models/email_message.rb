758b83e233b5c138a7c61827c517ceab722f5983
kilim choi <stevn1202@berkeley.edu>
2014-09-23 12:31:14 -0700

    chane

--- app/models/email_message.rb
+++ app/models/email_message.rb
@@ -10,6 +10,10 @@ class EmailMessage < ActiveRecord::Base
     puts "does it enter the delay email?"
     t = Time.now
     puts "time zone is ", Time.zone
+    puts "is gmt?", Time.now.gmt?
+    puts "is utc?", Time.now.utc?
+    puts "local time", Time.now.localtime
+    puts "time now without strftime", Time.now
     puts "time is ", t.getlocal.strftime("%H:%M")
     puts "time now is ", t.strftime("%H:%M")
     mailers = self.where("string_delivery_at = ?", Time.now.strftime("%H:%M"))
