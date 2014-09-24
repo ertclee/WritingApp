18667bc6517d59b87efab03dc5c03ca8f55ac6fa
kilim choi <stevn1202@berkeley.edu>
2014-09-23 18:56:34 -0700

    asset compile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,21 +33,5 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     "/writing_challenges/1/responses/new"
   end
-  around_filter :set_time_zone
-                                                                                 
-  private
-                                                                                   
-  def set_time_zone
-    old_time_zone = Time.zone
-    puts "enters here"
-    puts "browser_timezone.present? ", browser_timezone.present?
-    Time.zone = browser_timezone if browser_timezone.present?
-    yield
-  ensure
-    Time.zone = old_time_zone
-  end
-                                                                                   
-  def browser_timezone
-    cookies["browser.timezone"]
-  end
+  
 end
