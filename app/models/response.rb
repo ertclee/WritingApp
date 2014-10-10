51d9ac08adf94fffdce26d2a922f43a5c53d41ca
liuben10 <benjamindliu@yahoo.com>
2014-10-09 22:04:05 -0700

    Changed to use Steven's edit distance algorithm

--- app/models/response.rb
+++ app/models/response.rb
@@ -5,29 +5,26 @@ class Response < ActiveRecord::Base
 	has_many :edits
 
 	def compute_the_difference_score_between_two_strings(new_string)
-	  oldstringarr = self.response.split
-	  newstringarr = new_string.split
-    m = newstringarr.length
-    n = oldstringarr.length
-    return m if n == 0
-    return n if m == 0
-    d = Array.new(m+1) {Array.new(n+1)}
-  
-    (0..m).each {|i| d[i][0] = i}
-    (0..n).each {|j| d[0][j] = j}
-    (1..n).each do |j|
-      (1..m).each do |i|
-        d[i][j] = if oldstringarr[i-1] == newstringarr[j-1]  # adjust index into string
-                    d[i-1][j-1]       # no operation required
-                  else
-                    [ d[i-1][j]+1,    # deletion
-                      d[i][j-1]+1,    # insertion
-                      d[i-1][j-1]+1,  # substitution
-                    ].min
-                  end
+    deletion = 0
+    first = self.response.split
+    second = new_string.split
+    first_size = first.size
+    second_size = second.size
+    matrix = [(0..first_size).to_a]
+    (1..second_size ).each do |j|
+      matrix << [j] + [0] * (first_size)
+    end
+    count = 0
+    (1..second_size).each do |i|
+      (1..first_size).each do |j|
+        if first[j-1] == second[i-1]
+          matrix[i][j] = matrix[i-1][j-1]
+        else
+          matrix[i][j] = [matrix[i-1][j],matrix[i][j-1], matrix[i-1][j-1]].min + 1
+        end
       end
     end
-    d[m][n]
+    return matrix.last.last 
 	end
 
 	def compute_word_count_with_edits(difference_score_for_new_word, difference_score_for_old_word)
