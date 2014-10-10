c2e8a8e1cb0607aa58930c0eceaa2429bf4043db
liuben10 <benjamindliu@yahoo.com>
2014-10-09 18:56:31 -0700

    undid steven's refactoring.

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,32 +26,110 @@ class User < ActiveRecord::Base
     count
   end
   
-  def words_for_day(date, responses)
+   def words_for_day date
     @wordcount = 0
-    responses.each do |response| 
-      if response.created_at.strftime('%d-%m-%Y') == date.strftime('%d-%m-%Y')
-        @wordcount += response.wordcount.to_i
-        @edit_for_response = Edit.where("response_id = ? AND user_id = ? ", response.id, 1)
-         if @edit_for_response.present? 
-           @wordcount -= @edit_for_response[0].difference
-         end
-      else
-         @current_user_id = 1 #replace this with current_user id
-         @edit_for_response = Edit.where("response_id = ? AND user_id = ? AND time = ?", response.id, 1, date.strftime("%Y-%m-%d"))
-         puts @edit_for_response 
-         if @edit_for_response
-           @wordcount += @edit_for_response[0].difference
-         end
+    @edits = edits_on_same_date date
+    @responses = responses_on_same_date date
+    @editcount = 0
+    @responsecount = 0
+    @edits.each do |edit|
+      @editcount += edit.difference
+    end
+    @responses.each do |response|
+      @responsecount += response.wordcount.to_i
+    end
+    @wordcount = @editcount + @responsecount
+    return @wordcount
+  end
+
+#REFACTOR
+  def words_by_day(start_date)
+    number_of_words = Hash.new
+    already_calculated_edits = Hash.new
+    (start_date..start_date + 1.month).each { |d| number_of_words["#{d.strftime('%d-%m-%Y')}"] = words_for_day(d) }
+#    self.responses.each { |response|
+#      numedits = 0
+#      @edits = self.edits_on_same_date response.time
+#      @edits.each do |edit|
+#        print "STARTING EDIT SEARCH"
+#        if !already_calculated_edits.has_key?(edit.id)
+#          numedits += edit.difference
+#          already_calculated_edits[edit.id] = 1
+#        end
+#      end
+#      number_of_words["#{response.time.to_date.strftime('%d-%m-%Y')}"] += (response.wordcount.to_i + numedits)
+#    }
+    number_of_words
+  end
+  
+   def responses_on_same_date date
+    print "STARTIN SEARCH RESPONSES\n"
+    @responses = []
+    @allresponses = Response.all
+    if @allresponses == nil
+      return @responses
+    end
+    @allresponses.each do |response|
+      print "RESULT: " + response.inspect
+      if response.time != nil
+        if response.time.strftime('%d-%m-%Y') == date.strftime('%d-%m-%Y')
+          @responses.push(response)
+        end
       end
     end
-    @wordcount
+    print @responses.inspect
+    print "END FOUND ALL RESPONSES\n"
+    @responses
   end
+  
+  
+  def edits_on_same_date date
+    print "STARTIN SEARCH\n"
+    @edits = []
+    @alledits = Edit.all
+    if @alledits == nil
+      return @edits
+    end
+    @alledits.each do |edit|
+      print "RESULT: " + edit.inspect
+      if edit.time != nil
+        if edit.time.strftime('%d-%m-%Y') == date.strftime('%d-%m-%Y')
+          @edits.push(edit)
+        end
+      end
+    end
+    print @edits.inspect
+    print "END FOUND ALL EDITS\n"
+    @edits
+  end
+
+  
+ #def words_for_day(date, responses)
+ #   @wordcount = 0
+ #   responses.each do |response| 
+ #     if response.created_at.strftime('%d-%m-%Y') == date.strftime('%d-%m-%Y')
+ #       @wordcount += response.wordcount.to_i
+ #       @edit_for_response = Edit.where("response_id = ? AND user_id = ? ", response.id, 1)
+ #        if @edit_for_response.present? 
+ #          @wordcount -= @edit_for_response[0].difference
+ #        end
+ #     else
+ #        @current_user_id = 1 #replace this with current_user id
+  #       @edit_for_response = Edit.where("response_id = ? AND user_id = ? AND time = ?", response.id, 1, date.strftime("%Y-%m-%d"))
+  #       puts @edit_for_response 
+  #       if @edit_for_response
+  #         @wordcount += @edit_for_response[0].difference
+  #       end
+  #    end
+  #  end
+  #  @wordcount
+  #end
 
   # REFACTOR
   def words_by_day(start_date)
     number_of_words = Hash.new
     @responses = Response.all
-    (start_date..(start_date + 1.month - 1.day)).each { |d| number_of_words["#{d.strftime('%m-%d-%Y')}"] = words_for_day(d, @responses) }
+    (start_date..(start_date + 1.month - 1.day)).each { |d| number_of_words["#{d.strftime('%m-%d-%Y')}"] = words_for_day(d) }
     number_of_words
   end
 
